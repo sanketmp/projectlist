6b735793cd8f0ead74a467a9fd8a0331f4782532
sanketmp <sanketmpol@gmail.com>
2024-04-20 16:13:18 +0530

    3rd commit

--- src/components/data.js
+++ src/components/data.js
@@ -2,7 +2,7 @@ export const projectsArray = [
   {
     title: "triviosa.",
     techStack: "React, Material UI, Redux Toolkit, Thunk.",
-    imgSrc: "./src/assets/triviosa.webp",
+    imgSrc: ".assets/triviosa.webp",
     desc: "A Quiz app. This app will render 10 random questions on Computer Category.",
     githubLink: "https://github.com/sanketmp/triviosa2",
     demoLink: "https://trytriviosa.netlify.app/",
@@ -10,7 +10,7 @@ export const projectsArray = [
   {
     title: "Prakriti Patra",
     techStack: "Vanilla JS, custom API.",
-    imgSrc: "./src/assets/prakriti.webp",
+    imgSrc: ".assets/prakriti.webp",
     desc: "An indigenous webapp that let's you check to weather online for free.",
     githubLink: "https://github.com/sanketmp/prakritipatra",
     demoLink: "https://prakritipatra.netlify.app/",
@@ -18,7 +18,7 @@ export const projectsArray = [
   {
     title: "More on the way...",
     techStack: "",
-    imgSrc: "./src/assets/bg.webp",
+    imgSrc: ".assets/bg.webp",
     desc: "",
     demoLink: "",
   },
